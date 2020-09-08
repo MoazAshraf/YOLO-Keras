1d34604fc5513618dee212124f333645f76872e4
Moaz Ashraf <mouadhashraf@gmail.com>
2020-09-09 00:02:12 +0200

    Build the YOLO model

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# YOLO Implementation in Keras
-YOLO implementation from scratch in Keras.
+# YOLO Implementation in Keras (TensorFlow 2)
+In this project, I attempt to implement and train YOLO (v1) as described in the paper [You Only Look Once](https://arxiv.org/pdf/1506.02640.pdf) using TensorFlow 2's Keras API implementation.
 
-# License
+## YOLO Architecture
+<div style="text-align: center;">
+<img src="yolo-architecture.png" height="400">
+</div>
+
+## Dependencies
+All the packages used in this project are in the [requirements.txt](requirements.txt) file. For example, you can use pip to install them in a virtual environment like so:
+
+```
+pip install -r requirements.txt
+```
+
+## License
 MIT License, check the [LICENSE](LICENSE) file.
---
+++ yolo.py
@@ -0,0 +1,81 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, LeakyReLU, Flatten, Dense, Reshape
+
+
+
+def create_model(input_shape=(448, 448, 3), lrelu_alpha=0.1, s=7, b=2, c=20):
+    """
+    Creates the YOLO model using tf.keras as desribed by the paper You Only Look Once.
+
+    The model expects preprocessed images of shape (448, 448, 3) and outputs predictions
+    of the shape (7, 7, 30). S = 7, B = 2, C = 20.
+    """
+
+
+    output_shape=(s, s, b * 5 + c)
+
+    model = tf.keras.models.Sequential()
+
+    model.add(Conv2D(64, kernel_size=7, strides=2, padding='same', input_shape=input_shape))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(MaxPooling2D(2, 2, padding='same'))
+
+    model.add(Conv2D(192, kernel_size=3, strides=1, padding='same'))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(MaxPooling2D(2, 2, padding='same'))
+
+    model.add(Conv2D(128, kernel_size=1, strides=1, padding='same'))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(Conv2D(256, kernel_size=3, strides=1, padding='same'))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(Conv2D(256, kernel_size=1, strides=1, padding='same'))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(Conv2D(512, kernel_size=3, strides=1, padding='same'))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(MaxPooling2D(2, 2, padding='same'))
+
+    for _ in range(4):
+        model.add(Conv2D(256, kernel_size=1, strides=1, padding='same'))
+        model.add(LeakyReLU(alpha=lrelu_alpha))
+        model.add(Conv2D(512, kernel_size=3, strides=1, padding='same'))
+        model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(Conv2D(512, kernel_size=1, strides=1, padding='same'))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(Conv2D(1024, kernel_size=3, strides=1, padding='same'))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(MaxPooling2D(2, 2, padding='same'))
+
+    for _ in range(2):
+        model.add(Conv2D(512, kernel_size=1, strides=1, padding='same'))
+        model.add(LeakyReLU(alpha=lrelu_alpha))
+        model.add(Conv2D(1024, kernel_size=3, strides=1, padding='same'))
+        model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(Conv2D(1024, kernel_size=3, strides=1, padding='same'))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+    model.add(Conv2D(1024, kernel_size=3, strides=2, padding='same'))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+
+    for _ in range(2):
+        model.add(Conv2D(1024, kernel_size=3, strides=1, padding='same'))
+        model.add(LeakyReLU(alpha=lrelu_alpha))
+
+    model.add(Flatten())
+
+    model.add(Dense(512))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+
+    model.add(Dense(4096))
+    model.add(LeakyReLU(alpha=lrelu_alpha))
+
+    model.add(Dense(np.prod(output_shape)))
+    model.add(Reshape(output_shape))
+
+    return model
+
+if __name__ == "__main__":
+    yolo_model = create_model()
+    yolo_model.summary()
